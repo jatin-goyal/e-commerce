5067eda8574dfe91b82f259678ae958fe597f1f9
Jatin Goyal <25JG2001@GMAIL.COM>
2022-06-28 01:50:13 +0530

    HeroBanner skeleton

---
+++ components/index.js
@@ -0,0 +1,7 @@
+export { default as Navbar } from "./navbar";
+export { default as Footer } from "./footer";
+export { default as FooterBanner } from "./footerBanner";
+export { default as HeroBanner } from "./heroBanner";
+export { default as Cart } from "./Cart";
+export { default as Layout } from "./Layout";
+export { default as Product } from "./Product";
