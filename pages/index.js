746dd64412d3cd556df93b5dbbd4eb4a8e72ff78
Jatin Goyal <25JG2001@GMAIL.COM>
2022-06-28 17:04:08 +0530

    product & footerBanner component

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,19 @@ const Home = ({ products, bannerData }) => {
   return (
     <>
       <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+
       <div className="products-heading">
         <h2>Best selling products</h2>
         <p>Speakers of various types</p>
       </div>
+
       <div className="products-container">
-        {products?.map((product) => product.name)}
+        {products?.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
       </div>
-      <FooterBanner />
+
+      <FooterBanner footerBanner={bannerData && bannerData[0]} />
     </>
   );
 };
