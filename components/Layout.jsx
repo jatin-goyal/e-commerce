df802bca86a332637983a9ca552f966a3db3413e
Jatin Goyal <25JG2001@GMAIL.COM>
2022-06-28 19:03:32 +0530

    Layout done

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import Head from "next/head";
+import NavBar from "./navbar";
+import Footer from "./footer";
 
-const Layout = () => {
-  return <div>Layout</div>;
+const Layout = ({ children }) => {
+  return (
+    <div className="layout">
+      <Head>
+        <title>Boat Store</title>
+      </Head>
+
+      <header>
+        <NavBar />
+      </header>
+
+      <main className="main-container">{children}</main>
+
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 };
 
 export default Layout;
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Link from "next/link";
+import { AiOutlineShopping } from "react-icons/ai";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <div className="navbar-container">
+      <p className="logo">
+        <Link href="/">Boat headphones </Link>
+      </p>
+
+      <button type="button" className="cart-icon" onClick="">
+        <AiOutlineShopping />
+        <span className="cart-item-qty">10</span>
+      </button>
+    </div>
+  );
 };
 
 export default NavBar;
