0525e9a6b1b369caf97c307b6ef31b034ef02f9c
Jatin Goyal <25JG2001@GMAIL.COM>
2022-06-30 20:47:08 +0530

    capitalising files

--- components/index.js
+++ components/index.js
@@ -1,7 +1,7 @@
-export { default as Navbar } from "./navbar";
-export { default as Footer } from "./footer";
-export { default as FooterBanner } from "./footerBanner";
-export { default as HeroBanner } from "./heroBanner";
+export { default as Navbar } from "./Navbar";
+export { default as Footer } from "./Footer";
+export { default as FooterBanner } from "./FooterBanner";
+export { default as HeroBanner } from "./HeroBanner";
 export { default as Cart } from "./Cart";
 export { default as Layout } from "./Layout";
 export { default as Product } from "./Product";
